a82dffadd00a236483f8074032abf241537c0872
Bogdan Floris <bogdan.floris@gmail.com>
2023-09-14 15:13:16 +0300

    feat: add nvim surround

--- files/.config/nvim/lua/custom/plugins.lua
+++ files/.config/nvim/lua/custom/plugins.lua
@@ -184,6 +184,16 @@ local plugins = {
   },
   { "tpope/vim-dadbod", lazy = false },
   { "kristijanhusak/vim-dadbod-ui", lazy = false },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup {
+        -- Configuration here, or leave empty to use defaults
+      }
+    end,
+  },
 }
 
 return plugins
