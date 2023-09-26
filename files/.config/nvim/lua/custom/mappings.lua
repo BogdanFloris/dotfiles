52b689e885027f3d3a9ecd256f41db07b24c7c01
BogdanFloris <bogdan.floris@gmail.com>
2023-09-26 14:51:59 +0300

    feat: add harpoon and persistence

--- files/.config/nvim/lua/custom/mappings.lua
+++ files/.config/nvim/lua/custom/mappings.lua
@@ -7,18 +7,129 @@ M.disabled = {
 
   n = {
     ["<A-h>"] = "",
+    ["<leader>v"] = "",
   },
 }
 
-M.abc = {
+M.telescope = {
   n = {
     ["<leader>cc"] = { "<cmd> Telescope <CR>", "Telescope" },
     ["gr"] = { "<cmd> Telescope lsp_references <CR>", "Telescope LSP references" },
     ["gi"] = { "<cmd> Telescope lsp_implementations <CR>", "Telescope LSP implementations" },
     ["gd"] = { "<cmd> Telescope lsp_definitions <CR>", "Telescope LSP definitions" },
-    ["<C-q>"] = { "<cmd> q <CR>", "Quit" },
+  },
+}
+
+M.typescript = {
+  n = {
     ["<leader>co"] = { "<cmd>TypescriptOrganizeImports<CR>", "Typescript Organize Imports" },
     ["<leader>cR"] = { "<cmd>TypescriptRenameFile<CR>", "Typescript Rename File" },
+  },
+}
+
+M.general = {
+  n = {
+    ["<C-q>"] = { "<cmd> q <CR>", "Quit" },
+  },
+}
+
+M.harpoon = {
+  n = {
+    ["<leader>h"] = {
+      function()
+        require("harpoon.ui").toggle_quick_menu()
+      end,
+      "Toggle quick menu",
+    },
+    ["<leader>hc"] = {
+      function()
+        require("harpoon.mark").clear_all()
+      end,
+      "Clear all marks",
+    },
+    ["<leader>ha"] = {
+      function()
+        require("harpoon.mark").add_file()
+      end,
+      "Add file",
+    },
+    ["<leader>1"] = {
+      function()
+        require("harpoon.ui").nav_file(1)
+      end,
+      "Navigate to file 1",
+    },
+    ["<leader>2"] = {
+      function()
+        require("harpoon.ui").nav_file(2)
+      end,
+      "Navigate to file 2",
+    },
+    ["<leader>3"] = {
+      function()
+        require("harpoon.ui").nav_file(3)
+      end,
+      "Navigate to file 3",
+    },
+    ["<leader>4"] = {
+      function()
+        require("harpoon.ui").nav_file(4)
+      end,
+      "Navigate to file 4",
+    },
+    ["<leader>5"] = {
+      function()
+        require("harpoon.ui").nav_file(5)
+      end,
+      "Navigate to file 5",
+    },
+    ["<leader>6"] = {
+      function()
+        require("harpoon.ui").nav_file(6)
+      end,
+      "Navigate to file 6",
+    },
+    ["<leader>7"] = {
+      function()
+        require("harpoon.ui").nav_file(7)
+      end,
+      "Navigate to file 7",
+    },
+    ["<leader>8"] = {
+      function()
+        require("harpoon.ui").nav_file(8)
+      end,
+      "Navigate to file 8",
+    },
+    ["<leader>9"] = {
+      function()
+        require("harpoon.ui").nav_file(9)
+      end,
+      "Navigate to file 9",
+    },
+  },
+}
+
+M.persistence = {
+  n = {
+    ["<leader>qs"] = {
+      function()
+        require("persistence").load()
+      end,
+      "Restore session",
+    },
+    ["<leader>ql"] = {
+      function()
+        require("persistence").load { last = true }
+      end,
+      "Restore last session",
+    },
+    ["<leader>qd"] = {
+      function()
+        require("persistence").stop()
+      end,
+      "Stop persistence",
+    },
   },
 }
 
--- files/.config/nvim/lua/custom/plugins.lua
+++ files/.config/nvim/lua/custom/plugins.lua
@@ -238,6 +238,14 @@ local plugins = {
       }
     end,
   },
+  { "ThePrimeagen/harpoon", dependencies = "nvim-lua/plenary.nvim" },
+  {
+    "folke/persistence.nvim",
+    event = "BufReadPre", -- this will only start session saving when an actual file was opened
+    opts = {
+      -- add any custom options here
+    },
+  },
 }
 
 return plugins
