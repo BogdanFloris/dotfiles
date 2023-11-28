e1161ec3d73291258aa87972ddeeb9a9c21480d9
BogdanFloris <bogdan.floris@gmail.com>
2023-11-28 10:56:42 +0200

    feat: add oil nvim

--- files/.config/nvim/init.lua
+++ files/.config/nvim/init.lua
@@ -182,6 +182,17 @@ require('lazy').setup({
   },
 
   {
+    -- File tree
+    'stevearc/oil.nvim',
+    opts = {},
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('oil').setup {}
+      vim.keymap.set('n', '<leader>-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+    end,
+  },
+
+  {
     -- Add indentation guides even on blank lines
     'lukas-reineke/indent-blankline.nvim',
     -- Enable `lukas-reineke/indent-blankline.nvim`
