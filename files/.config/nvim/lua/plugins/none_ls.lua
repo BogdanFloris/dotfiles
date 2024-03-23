807ddd2ba32c00d45d355cb337994b5a48e35936
BogdanFloris <bogdan.floris@gmail.com>
2024-03-23 10:07:01 +0200

    feat: remove old formatters and linters

--- files/.config/nvim/lua/plugins/none_ls.lua
+++ files/.config/nvim/lua/plugins/none_ls.lua
@@ -22,22 +22,17 @@ return {
         -- formatters
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettierd,
-        null_ls.builtins.formatting.eslint_d,
         null_ls.builtins.formatting.djlint,
         null_ls.builtins.formatting.rustywind,
-        null_ls.builtins.formatting.ruff,
         null_ls.builtins.formatting.shfmt,
         null_ls.builtins.formatting.gofumpt,
         null_ls.builtins.formatting.goimports,
         null_ls.builtins.formatting.golines,
         null_ls.builtins.formatting.sql_formatter,
-        null_ls.builtins.formatting.rustfmt,
         null_ls.builtins.formatting.clang_format,
 
         -- diagnostics
-        null_ls.builtins.diagnostics.shellcheck,
         null_ls.builtins.diagnostics.vale,
-        null_ls.builtins.diagnostics.jsonlint,
         null_ls.builtins.diagnostics.djlint,
         null_ls.builtins.diagnostics.hadolint,
         null_ls.builtins.diagnostics.sqlfluff,
