7fe54d4abd818881bf59feea9de9539f6afa3896
Bogdan Floris <bogdan.floris@gmail.com>
2025-02-12 11:05:41 +0200

    feat: switch to fish shell

---
+++ files/.config/fish/config.fish
@@ -0,0 +1,42 @@
+/opt/homebrew/bin/brew shellenv | source
+
+set -x LANG "en_US.UTF-8"
+set -x SSH_KEY_PATH "~/.ssh/rsa_id"
+
+fish_add_path /Users/bogdan/go/bin
+fish_add_path /opt/homebrew/bin
+fish_add_path /opt/homebrew/opt/openjdk/bin
+fish_add_path $HOME/.poetry/bin
+fish_add_path $HOME/.zvm/self
+fish_add_path $HOME/.zvm/bin
+fish_add_path $HOME/.yarn/bin
+fish_add_path $HOME/.config/yarn/global/node_modules/.bin
+fish_add_path /opt/homebrew/opt/postgresql@16/bin
+fish_add_path $HOME/.local/bin
+
+alias lg='lazygit'
+alias ld='lazydocker'
+alias cat='bat'
+alias maelstrom='~/maelstrom/maelstrom'
+alias chrome-no-sec='/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --user-data-dir="/tmp/james_bonds_browser" --disable-web-security'
+
+# Load environment variables from .env file if it exists
+if test -f ~/.config/fish/conf.d/env.fish
+    source ~/.config/fish/conf.d/env.fish
+end
+
+set -x PNPM_HOME "$HOME/Library/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+    fish_add_path $PNPM_HOME
+end
+pyenv init - | source
+status --is-interactive; and rbenv init - fish | source
+fnm env --use-on-cd | source
+
+starship init fish | source
+atuin init fish | source
+
+function fish_user_key_bindings
+    # Use Cmd+Space to accept autosuggestion
+    bind -k nul forward-char
+end
---
+++ fisher-plugins.fish
@@ -0,0 +1,20 @@
+#!/usr/bin/env fish
+
+# Install fisher if not already installed
+if not type -q fisher
+    curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source
+end
+
+# Install essential plugins
+fisher install \
+    jorgebucaran/autopair.fish \       # Auto-closing pairs
+    PatrickF1/fzf.fish \               # Fuzzy finding
+    meaningful-ooo/sponge \            # Pattern matching suggestion
+    franciscolourenco/done             # Notification on long-running commands
+    danhper/fish-ssh-agent \           # SSH agent integration
+    jhillyerd/plugin-git \             # Git abbreviations and utilities
+    jethrokuan/z \                     # Directory jumping (like autojump/fasd)
+    gazorby/fish-abbreviation-tips \   # Shows abbreviation suggestions as you type
+    nickeb96/puffer-fish \             # Adds better expandable aliases
+    patrickf1/colored_man_pages.fish \ # Colored man pages
+    jomik/fish-gruvbox \               # Gruvbox color scheme for fish
