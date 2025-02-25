fe165f74dba0692a66f1eac542a444a35ba2243b
Bogdan Floris <bogdan.floris@gmail.com>
2025-02-25 07:36:00 +0200

    feat: add render markdown back

--- files/.config/nvim/init.lua
+++ files/.config/nvim/init.lua
@@ -368,6 +368,15 @@ require('lazy').setup({
     opts = {},
     cmd = { 'Typr', 'TyprStats' },
   },
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+  },
 
   require 'plugins.none_ls',
   require 'plugins.harpoon',
