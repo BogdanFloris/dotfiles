9aad7fa0e3d6653fc36f5cb5faae9c53df6aa317
Bogdan Floris <bogdan.floris@gmail.com>
2025-11-11 21:09:51 +0200

    feat: neovim config

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,758 @@
+-- init.lua
+-- @BogdanFloris
+--
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- [[ Install `lazy.nvim` plugin manager ]]
+--    https://github.com/folke/lazy.nvim
+--    `:help lazy.nvim.txt` for more info
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.filetype.add({
+	extension = {
+		gotmpl = "gotmpl",
+		templ = "templ",
+		wgsl = "wgsl",
+		-- zig.zon filetype is not supported by default
+		zon = "zig",
+	},
+	filename = {},
+	pattern = {
+		["*.Dockerfile.*"] = "dockerfile",
+		["*/templates/**/*.html"] = "htmldjango",
+	},
+})
+
+vim.g.vimwiki_list = {
+	{
+		path = "~/daily/",
+		syntax = "markdown",
+		ext = "md",
+		diary_rel_path = ".",
+		diary_header = "# Daily List",
+	},
+}
+
+require("lazy").setup({
+	-- Git related plugins
+	"tpope/vim-fugitive",
+	"tpope/vim-rhubarb",
+
+	-- Detect tabstop and shiftwidth automatically
+	"tpope/vim-sleuth",
+
+	-- Buf del
+	"ojroques/nvim-bufdel",
+
+	{
+		"vimwiki/vimwiki",
+		event = "BufEnter *.md",
+		keys = { "<leader>ww", "<leader>wt" },
+	},
+
+	{
+		-- Gruvbox Theme
+		"ellisonleao/gruvbox.nvim",
+		priority = 1000,
+		config = function()
+			require("gruvbox").setup({
+				contrast = "hard",
+			})
+			vim.cmd.colorscheme("gruvbox")
+		end,
+	},
+
+	{
+		-- LSP Configuration & Plugins
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			{ "mason-org/mason.nvim", opts = {} },
+			{ "j-hui/fidget.nvim",    opts = {} },
+		},
+	},
+
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true,
+	},
+
+	{
+		"folke/lazydev.nvim",
+		ft = "lua", -- only load on lua files
+		opts = {
+			library = {
+				-- See the configuration section for more details
+				-- Load luvit types when the `vim.uv` word is found
+				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+			},
+		},
+	},
+
+	{
+		-- Persistence to restore sessions
+		"folke/persistence.nvim",
+		event = "BufReadPre", -- this will only start session saving when an actual file was opened
+		opts = {},
+	},
+
+	{
+		-- Nvim Surround
+		"kylechui/nvim-surround",
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({})
+		end,
+	},
+
+	-- Useful plugin to show you pending keybinds.
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		opts = {},
+		keys = {
+
+			{ "<leader>c",  group = "[C]ode" },
+			{ "<leader>c_", hidden = true },
+			{ "<leader>d",  group = "[D]ocument" },
+			{ "<leader>d_", hidden = true },
+			{ "<leader>g",  group = "[G]it" },
+			{ "<leader>g_", hidden = true },
+			{ "<leader>h",  group = "[H]arpoon" },
+			{ "<leader>h_", hidden = true },
+			{ "<leader>p",  group = "More git" },
+			{ "<leader>p_", hidden = true },
+			{ "<leader>r",  group = "[R]ename" },
+			{ "<leader>r_", hidden = true },
+			{ "<leader>s",  group = "[S]earch" },
+			{ "<leader>s_", hidden = true },
+			{ "<leader>w",  group = "[W]orkspace" },
+			{ "<leader>w_", hidden = true },
+		},
+	},
+	{
+		-- Adds git related signs to the gutter, as well as utilities for managing changes
+		"lewis6991/gitsigns.nvim",
+		opts = {
+			-- See `:help gitsigns.txt`
+			signs = {
+				add = { text = "+" },
+				change = { text = "~" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+			},
+			on_attach = function(bufnr)
+				vim.keymap.set(
+					"n",
+					"<leader>ph",
+					require("gitsigns").preview_hunk,
+					{ buffer = bufnr, desc = "Preview git hunk" }
+				)
+				vim.keymap.set(
+					"n",
+					"<leader>rh",
+					require("gitsigns").reset_hunk,
+					{ buffer = bufnr, desc = "Reset git hunk" }
+				)
+				vim.keymap.set(
+					"n",
+					"<leader>bh",
+					require("gitsigns").blame_line,
+					{ buffer = bufnr, desc = "Blame git line" }
+				)
+
+				-- don't override the built-in and fugitive keymaps
+				local gs = package.loaded.gitsigns
+				vim.keymap.set({ "n", "v" }, "]c", function()
+					if vim.wo.diff then
+						return "]c"
+					end
+					vim.schedule(function()
+						gs.next_hunk()
+					end)
+					return "<Ignore>"
+				end, { expr = true, buffer = bufnr, desc = "Jump to next hunk" })
+				vim.keymap.set({ "n", "v" }, "[c", function()
+					if vim.wo.diff then
+						return "[c"
+					end
+					vim.schedule(function()
+						gs.prev_hunk()
+					end)
+					return "<Ignore>"
+				end, { expr = true, buffer = bufnr, desc = "Jump to previous hunk" })
+			end,
+		},
+	},
+
+	{
+		-- Vim Tmux Navigator
+		"christoomey/vim-tmux-navigator",
+		event = "VeryLazy",
+	},
+
+	{
+		-- Set lualine as statusline
+		"nvim-lualine/lualine.nvim",
+		-- See `:help lualine.txt`
+		opts = {
+			options = {
+				icons_enabled = false,
+				theme = "gruvbox",
+				component_separators = "|",
+				section_separators = "",
+			},
+		},
+	},
+
+	{
+		-- File tree
+		"stevearc/oil.nvim",
+		opts = {},
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("oil").setup({})
+			vim.keymap.set("n", "<leader>-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+		end,
+	},
+
+	{
+		-- Add indentation guides even on blank lines
+		"lukas-reineke/indent-blankline.nvim",
+		-- Enable `lukas-reineke/indent-blankline.nvim`
+		-- See `:help ibl`
+		main = "ibl",
+		opts = {},
+	},
+
+	-- "gc" to comment visual regions/lines
+	{ "numToStr/Comment.nvim", opts = {} },
+
+	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {
+			search = {
+				-- keeping this here in case I have a big project and want to narrow the search
+				pattern = [[\b(KEYWORDS):]],
+			},
+		},
+	},
+
+	-- Fuzzy Finder (files, lsp, etc)
+	{
+		"nvim-telescope/telescope.nvim",
+		branch = "master",
+		event = "VimEnter",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			-- Fuzzy Finder Algorithm which requires local dependencies to be built.
+			-- Only load if `make` is available. Make sure you have the system
+			-- requirements installed.
+			{
+				"nvim-telescope/telescope-fzf-native.nvim",
+				build = "make",
+				cond = function()
+					return vim.fn.executable("make") == 1
+				end,
+			},
+			{ "nvim-telescope/telescope-ui-select.nvim" },
+		},
+	},
+
+	{
+		"kevinhwang91/nvim-ufo",
+		dependencies = { "kevinhwang91/promise-async" },
+		config = function()
+			require("ufo").setup({
+				---@diagnostic disable-next-line: unused-local
+				provider_selector = function(bufnr, filetype, buftype)
+					return { "treesitter", "indent" }
+				end,
+			})
+
+			vim.keymap.set("n", "zR", require("ufo").openAllFolds, { desc = "Open all folds" })
+			vim.keymap.set("n", "zM", require("ufo").closeAllFolds, { desc = "Close all folds" })
+		end,
+	},
+
+	{
+		-- Highlight, edit, and navigate code
+		"nvim-treesitter/nvim-treesitter",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter-textobjects",
+			"windwp/nvim-ts-autotag",
+		},
+		build = ":TSUpdate",
+	},
+
+	{
+		"folke/snacks.nvim",
+		priority = 1000,
+		lazy = false,
+		---@type snacks.Config
+		opts = {
+			bigfile = { enabled = true },
+			input = { enabled = true },
+			quickfile = { enabled = true },
+			scope = { enabled = true },
+			scroll = { enabled = true },
+		},
+	},
+
+	{
+		"folke/trouble.nvim",
+		opts = {},
+		cmd = "Trouble",
+		keys = {
+			{
+				"<leader>xx",
+				"<cmd>Trouble diagnostics toggle<cr>",
+				desc = "Diagnostics (Trouble)",
+			},
+			{
+				"<leader>xX",
+				"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+				desc = "Buffer Diagnostics (Trouble)",
+			},
+			{
+				"<leader>cs",
+				"<cmd>Trouble symbols toggle focus=false<cr>",
+				desc = "Symbols (Trouble)",
+			},
+			{
+				"<leader>cl",
+				"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+				desc = "LSP Definitions / references / ... (Trouble)",
+			},
+			{
+				"<leader>xL",
+				"<cmd>Trouble loclist toggle<cr>",
+				desc = "Location List (Trouble)",
+			},
+			{
+				"<leader>xQ",
+				"<cmd>Trouble qflist toggle<cr>",
+				desc = "Quickfix List (Trouble)",
+			},
+		},
+	},
+
+	{
+		"vhyrro/luarocks.nvim",
+		priority = 1000,
+		config = true,
+		opts = {
+			rocks = { "lua-curl", "nvim-nio", "mimetypes", "xml2lua" },
+		},
+	},
+	{
+		"nvzone/typr",
+		dependencies = "nvzone/volt",
+		opts = {},
+		cmd = { "Typr", "TyprStats" },
+	},
+	{
+		"pmizio/typescript-tools.nvim",
+		dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+		opts = {},
+	},
+
+	{
+		"Bekaboo/dropbar.nvim",
+		config = function()
+			local dropbar_api = require("dropbar.api")
+			vim.keymap.set("n", "<Leader>;", dropbar_api.pick, { desc = "Pick symbols in winbar" })
+			vim.keymap.set("n", "[;", dropbar_api.goto_context_start, { desc = "Go to start of current context" })
+			vim.keymap.set("n", "];", dropbar_api.select_next_context, { desc = "Select next context" })
+		end,
+	},
+
+	require("plugins.blink"),
+	require("plugins.none_ls"),
+	require("plugins.harpoon"),
+	require("plugins.rustancean"),
+	-- require("plugins.gopher"),
+	-- require("plugins.debug"),
+	-- require("plugins.neotest"),
+	-- require("plugins.leetcode"),
+}, {})
+
+-- [[ Setting options ]]
+-- See `:help vim.o`
+
+-- Set highlight on search
+vim.o.hlsearch = false
+
+-- Make line numbers default
+vim.wo.relativenumber = true
+
+-- Enable mouse mode
+vim.o.mouse = "a"
+
+-- Sync clipboard between OS and Neovim.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.o.clipboard = "unnamedplus"
+
+-- Enable break indent
+vim.o.breakindent = true
+
+-- Save undo history
+vim.o.undofile = true
+
+-- Case-insensitive searching UNLESS \C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Keep signcolumn on by default
+vim.wo.signcolumn = "yes"
+
+-- Decrease update time
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
+
+-- Set completeopt to have a better completion experience
+vim.o.completeopt = "menuone,noselect"
+
+-- Tabs
+vim.o.tabstop = 4
+vim.o.expandtab = true
+
+-- Folds
+vim.o.foldcolumn = "1"
+vim.o.foldlevel = 99
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
+
+-- views can only be fully collapsed with the global statusline
+vim.opt.laststatus = 3
+
+vim.o.termguicolors = true
+
+-- Enable Rest NVIM
+vim.g.rest_nvim = {}
+
+-- [[ Basic Keymaps ]]
+
+-- Keymaps for better default experience
+-- See `:help vim.keymap.set()`
+vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
+vim.keymap.set("n", "<C-q>", "<cmd> qa <CR>", { desc = "Close all and quit" })
+vim.keymap.set("n", "<C-x>", "<cmd> bd <CR>", { desc = "Close buffer" })
+vim.keymap.set("n", "<leader>bd", "<cmd> BufDel <CR>", { desc = "Close buffer and preserve layout" })
+
+-- Remap for dealing with word wrap
+vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+-- Diagnostic keymaps
+vim.keymap.set("n", "[d", function()
+	vim.diagnostic.jump({ count = -1, float = true })
+end, { desc = "Go to previous diagnostic message" })
+vim.keymap.set("n", "]d", function()
+	vim.diagnostic.jump({ count = 1, float = true })
+end, { desc = "Go to next diagnostic message" })
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
+
+-- Persistence keymaps
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>qs",
+	[[<cmd>lua require("persistence").load()<cr>]],
+	{ desc = "Restore session for cwd" }
+)
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>ql",
+	[[<cmd>lua require("persistence").load({ last = true })<cr>]],
+	{ desc = "Restore the last session" }
+)
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>qd",
+	[[<cmd>lua require("persistence").stop()<cr>]],
+	{ desc = "Stop persistence" }
+)
+
+-- Todo keymaps
+vim.keymap.set("n", "]t", function()
+	require("todo-comments").jump_next()
+end, { desc = "Next todo comment" })
+vim.keymap.set("n", "[t", function()
+	require("todo-comments").jump_prev()
+end, { desc = "Previous todo comment" })
+vim.keymap.set("n", "<leader>st", "<cmd>TodoTelescope<cr>", { desc = "Search todo comments" })
+
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+	group = highlight_group,
+	pattern = "*",
+})
+
+-- [[ Configure Telescope ]]
+-- See `:help telescope` and `:help telescope.setup()`
+require("telescope").setup({
+	defaults = {
+		mappings = {
+			i = {
+				["<C-u>"] = false,
+				["<C-d>"] = false,
+			},
+		},
+	},
+	extensions = {
+		["ui-select"] = {
+			require("telescope.themes").get_dropdown(),
+		},
+	},
+})
+
+-- Enable telescope fzf native, if installed
+pcall(require("telescope").load_extension, "fzf")
+pcall(require("telescope").load_extension, "ui-select")
+
+-- See `:help telescope.builtin`
+vim.keymap.set("n", "<leader>?", require("telescope.builtin").oldfiles, { desc = "[?] Find recently opened files" })
+vim.keymap.set("n", "<leader><space>", require("telescope.builtin").buffers, { desc = "[ ] Find existing buffers" })
+vim.keymap.set("n", "<leader>/", function()
+	-- You can pass additional configuration to telescope to change theme, layout, etc.
+	require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+		winblend = 10,
+		previewer = false,
+	}))
+end, { desc = "[/] Fuzzily search in current buffer" })
+
+vim.keymap.set("n", "<leader>gf", require("telescope.builtin").git_files, { desc = "Search [G]it [F]iles" })
+vim.keymap.set("n", "<leader>sf", require("telescope.builtin").find_files, { desc = "[S]earch [F]iles" })
+vim.keymap.set("n", "<leader>sh", require("telescope.builtin").help_tags, { desc = "[S]earch [H]elp" })
+vim.keymap.set("n", "<leader>sw", require("telescope.builtin").grep_string, { desc = "[S]earch current [W]ord" })
+vim.keymap.set("n", "<leader>sg", require("telescope.builtin").live_grep, { desc = "[S]earch by [G]rep" })
+vim.keymap.set("n", "<leader>sd", require("telescope.builtin").diagnostics, { desc = "[S]earch [D]iagnostics" })
+vim.keymap.set("n", "<leader>sr", require("telescope.builtin").resume, { desc = "[S]earch [R]esume" })
+
+-- [[ Configure Treesitter ]]
+-- See `:help nvim-treesitter`
+-- Defer Treesitter setup after first render to improve startup time of 'nvim {filename}'
+vim.defer_fn(function()
+	---@diagnostic disable-next-line: missing-fields
+	require("nvim-treesitter.configs").setup({
+		-- Add languages to be installed here that you want installed for treesitter
+		ensure_installed = {
+			"asm",
+			"comment",
+			"c",
+			"cpp",
+			"cmake",
+			"make",
+			"terraform",
+			"sql",
+			"zig",
+			"markdown",
+			"dockerfile",
+			"gitignore",
+			"graphql",
+			"java",
+			"kotlin",
+			"jq",
+			"regex",
+			"ruby",
+			"wgsl",
+			"glsl",
+			"go",
+			"gomod",
+			"gosum",
+			"gosum",
+			"lua",
+			"python",
+			"rust",
+			"toml",
+			"tsx",
+			"javascript",
+			"typescript",
+			"vimdoc",
+			"vim",
+			"bash",
+			"html",
+			"htmldjango",
+			"css",
+			"scss",
+			"json",
+			"http",
+			"xml",
+			"yaml",
+		},
+
+		-- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
+		auto_install = false,
+
+		highlight = { enable = true },
+		-- indent = { enable = true },
+		autotag = { enable = true },
+		incremental_selection = {
+			enable = true,
+			keymaps = {
+				init_selection = "<c-space>",
+				node_incremental = "<c-space>",
+				scope_incremental = "<c-s>",
+				node_decremental = "<M-space>",
+			},
+		},
+		textobjects = {
+			select = {
+				enable = true,
+				lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+				keymaps = {
+					-- You can use the capture groups defined in textobjects.scm
+					["aa"] = "@parameter.outer",
+					["ia"] = "@parameter.inner",
+					["af"] = "@function.outer",
+					["if"] = "@function.inner",
+					["ac"] = "@class.outer",
+					["ic"] = "@class.inner",
+				},
+			},
+			move = {
+				enable = true,
+				set_jumps = true, -- whether to set jumps in the jumplist
+				goto_next_start = {
+					["]m"] = "@function.outer",
+					["]]"] = "@class.outer",
+				},
+				goto_next_end = {
+					["]M"] = "@function.outer",
+					["]["] = "@class.outer",
+				},
+				goto_previous_start = {
+					["[m"] = "@function.outer",
+					["[["] = "@class.outer",
+				},
+				goto_previous_end = {
+					["[M"] = "@function.outer",
+					["[]"] = "@class.outer",
+				},
+			},
+		},
+	})
+end, 0)
+
+-- Setup LSP servers directly
+require("mason").setup({})
+local on_attach = require("lsp_tools").on_attach
+
+local lsps = {
+	{ "graphql" },
+	{
+		"gopls",
+		{
+			filetypes = { "go", "gomod", "gowork", "gotmpl" },
+			gopls = {
+				gofumpt = true,
+				codelenses = {
+					gc_details = false,
+					generate = true,
+					regenerate_cgo = true,
+					run_govulncheck = true,
+					test = true,
+					tidy = true,
+					upgrade_dependency = true,
+					vendor = true,
+				},
+				hints = {
+					assignVariableTypes = true,
+					compositeLiteralFields = true,
+					compositeLiteralTypes = true,
+					constantValues = true,
+					functionTypeParameters = true,
+					parameterNames = true,
+					rangeVariableTypes = true,
+				},
+				completeUnimported = true,
+				usePlaceholders = true,
+				staticcheck = true,
+				semanticTokens = true,
+				analyses = {
+					fieldalignment = true,
+					nilness = true,
+					unusedparams = true,
+					unusedwrite = true,
+					useany = true,
+				},
+			},
+		},
+	},
+	{
+		"basedpyright",
+		{
+			settings = {
+				pyright = {
+					disableOrganizeImports = true, -- Using Ruff
+				},
+			},
+		},
+	},
+	{ "ruff" },
+	{ "eslint" },
+	{
+		"html",
+		{ filetypes = { "html", "htmldjango" } },
+	},
+	{
+		"sqlls",
+		{ filetypes = { "sql", "psql" } },
+	},
+	{
+		"glsl_analyzer",
+		{ filetypes = { "glsl" } },
+	},
+	{ "nil_ls" },
+	{ "lua_ls" },
+	{
+		"zls",
+		{ filetypes = { "zig" } },
+	},
+	{
+		"clangd",
+		{
+			cmd = {
+				"clangd",
+				"--offset-encoding=utf-16",
+			},
+		},
+	},
+}
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require("blink.cmp").get_lsp_capabilities(capabilities)
+
+vim.lsp.config("*", {
+	capabilities = capabilities,
+	on_attach = on_attach,
+})
+for _, lsp in pairs(lsps) do
+	local name, config = lsp[1], lsp[2]
+	vim.lsp.enable(name)
+	if config then
+		vim.lsp.config(name, config)
+	end
+end
---
+++ nvim/.config/nvim/lua/plugins/none_ls.lua
@@ -0,0 +1,64 @@
+-- none-ls.nvim
+--
+-- Configuration for none-ls.nvim
+
+local lsp_formatting = function(bufnr)
+	vim.lsp.buf.format({
+		filter = function(client)
+			return client.name == "null-ls"
+		end,
+		bufnr = bufnr,
+	})
+end
+
+return {
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
+		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+		local sources = {
+			-- formatters
+			null_ls.builtins.formatting.stylua,
+			null_ls.builtins.formatting.prettier,
+			null_ls.builtins.formatting.djlint,
+			null_ls.builtins.formatting.rustywind,
+			null_ls.builtins.formatting.shfmt,
+			null_ls.builtins.formatting.gofumpt,
+			null_ls.builtins.formatting.goimports,
+			null_ls.builtins.formatting.golines,
+			null_ls.builtins.formatting.sql_formatter,
+			null_ls.builtins.formatting.clang_format,
+			null_ls.builtins.formatting.nixfmt,
+
+			-- diagnostics
+			null_ls.builtins.diagnostics.djlint,
+			null_ls.builtins.diagnostics.hadolint,
+			null_ls.builtins.diagnostics.sqlfluff.with({
+				extra_args = { "--dialect", "snowflake" },
+			}),
+
+			-- code actions
+			null_ls.builtins.code_actions.gomodifytags,
+			null_ls.builtins.code_actions.impl,
+		}
+
+		null_ls.setup({
+			sources = sources,
+
+			on_attach = function(client, bufnr)
+				if client.supports_method("textDocument/formatting") then
+					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = augroup,
+						buffer = bufnr,
+						callback = function()
+							lsp_formatting(bufnr)
+						end,
+					})
+				end
+			end,
+		})
+		vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, { desc = "LSP: Format" })
+	end,
+}
