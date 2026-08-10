91c41c0e50d6df0936f768538aaaacc9c23a9497
Bogdan Floris <bogdan.floris@gmail.com>
2026-08-10 20:49:54 +0300

    feat: add glow

--- nix-os/packages.nix
+++ nix-os/packages.nix
@@ -19,6 +19,7 @@ with pkgs; [
   diffnav
   difftastic
   mergiraf
+  glow
   atuin
   zoxide
   neovim
--- nix/flake.nix
+++ nix/flake.nix
@@ -43,6 +43,7 @@
             diffnav
             difftastic
             mergiraf
+            glow
             atuin
             zoxide
             neovim
